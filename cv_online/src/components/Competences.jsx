2ab8ea0b5bb68d03e78e29a0370f3548e61f268b
4lb4t0R <h4pt4sn7trir@gmail.com>
2022-11-30 17:01:56 +0100

    COMPETENCES OK

--- cv_online/src/components/Competences.jsx
+++ cv_online/src/components/Competences.jsx
@@ -4,10 +4,21 @@ import style from "../styles/Competences.module.css";
 function Competences() {
   return (
     <div className={style.pancarte}>
-      <p>MAGE</p>
-      <p>THIEF</p>
-      <p>WARRIOR</p>
-      <p>MONK</p>
+      <p>HTML</p>
+      <p>CSS+SASS</p>
+      <p>SEO naturel</p>
+      <p>JavaScript</p>
+      <p>Node JS</p>
+      <p>MongoDB</p>
+      <p>Express</p>
+      <p>React</p>
+      <p>Github</p>
+      <p>Anglais (C1-C2)</p>
+      <p>Capacités rédactionnelles</p>
+      <p>Adaptation du discours, gestion du public</p>
+      <p>Travail en équipe</p>
+      <p>Élaboration et suivi de projet</p>
+      <p>Capacités d'analyse et de recherches</p>
     </div>
   );
 }
