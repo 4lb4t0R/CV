e62e2183cf8fbde1e5e0176ae9e0cc4630e294a5
4lb4t0R <h4pt4sn7trir@gmail.com>
2022-11-30 11:15:53 +0100

    SQUELETTE EN PLACE

--- cv_online/src/components/MainMenu.jsx
+++ cv_online/src/components/MainMenu.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import style from "../styles/Mmenu.module.css";
+import style from "../styles/MainMenu.module.css";
 
 function menu() {
   return (
-    <div className={style.container}>
-      <p>Hello World</p>
-    </div>
+    <section className={style.container}>
+      <div>
+        <ul>
+            <li>Diplômes</li>
+            <li>Compétences</li>
+            <li>Centres d'intérêts</li>
+            <li>Contacts</li>
+        </ul>
+      </div>
+    </section>
   );
 }
 
