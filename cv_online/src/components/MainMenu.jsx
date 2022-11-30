93cde903112e10a30899458d475f38599fae72e0
4lb4t0R <h4pt4sn7trir@gmail.com>
2022-11-30 14:38:31 +0100

    OMG C'EST LINKED JSUIS TROP CON, AUTRES PAGES A LINK PUIS A DEV ET OOOK

--- cv_online/src/components/MainMenu.jsx
+++ cv_online/src/components/MainMenu.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import style from "../styles/MainMenu.module.css";
+import { Link } from "react-router-dom";
 
 function menu() {
   return (
     <section className={style.container}>
       <div className={style.pancarte}>
         <ul className={style.choix}>
-          <a href="Diplomes.jsx#diplomes">Diplômes</a>
+          <Link to="/diplômes">Diplômes</Link>
           <a href="Competences.jsx#competences">Compétences</a>
           <a href="CentreInterets.jsx#centresineters">Centres d'intérêts</a>
           <a href="Contact.jsx#contact">Contacts</a>
