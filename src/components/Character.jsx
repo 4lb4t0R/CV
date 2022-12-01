1538fa65c7f923720e6a3e910952fb0d2bb0191a
4lb4t0R <h4pt4sn7trir@gmail.com>
2022-12-01 10:49:05 +0100

    update readme

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -5,7 +5,7 @@ import pdp from "../assets/pdp.png";
 function Character() {
   return (
     <div className={style.pancarte}>
-      <img src={pdp} alt="photo représentative à 90%" />
+      <img src={pdp} alt="représentative à 90%" />
       <p>🔰DÉVELOPPEUR WEB FULLSTACK</p>
       <p>☎️06-84-98-17-09</p>
       <p>🟣LAFAYE NICOLAS</p>
